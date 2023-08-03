da6a8c5d9419452d730f488a25e4dbcab3684e67
Martin Alturria <martinalturria@hotmail.com>
2023-08-03 13:27:57 -0300

    add product model

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,73 @@
-export class Product {}
+import { Column, Model, Table, DataType } from 'sequelize-typescript';
+
+enum stateproduct {
+  Active = 'Activa',
+  Inactive = 'Inactiva',
+}
+
+@Table({
+  tableName: 'Products',
+  timestamps: true,
+  underscored: true,
+})
+export class Product extends Model<Product> {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  })
+  id: string;
+
+  @Column({
+    type: DataType.STRING(60),
+    allowNull: false,
+    validate: {
+      len: [1, 60],
+    },
+  })
+  title: string;
+
+  @Column({
+    type: DataType.TEXT,
+    allowNull: false,
+  })
+  description: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(stateproduct)),
+    allowNull: false,
+  })
+  state: stateproduct;
+
+  @Column({
+    type: DataType.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  })
+  stock: number;
+
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: true,
+  })
+  availability: number;
+
+  @Column({
+    type: DataType.ARRAY(DataType.STRING),
+    allowNull: true,
+  })
+  image: string[];
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  model: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  year: string;
+}
