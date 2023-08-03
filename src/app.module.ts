23b768e6ccf2815bd8b0a69de8095177e978e6d6
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-08-03 13:30:25 -0500

    Initialization of the CRUD User and creation of its entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import * as dotenv from 'dotenv'
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 dotenv.config()
 
 @Module({
@@ -18,6 +19,7 @@ dotenv.config()
       autoLoadModels: true,
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
