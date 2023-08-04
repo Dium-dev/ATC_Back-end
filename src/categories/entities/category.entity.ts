ec0e51c510c451246a998822f814a98483af850f
Lautaro Garcia <lautarogonzalogarcia@outlook.com>
2023-08-04 12:04:31 -0300

    Update category.entity.ts

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -2,7 +2,7 @@ import { Table, Model, Column, DataType } from 'sequelize-typescript';
 
 @Table({ 
   timestamps: false,
-  tableName: 'categories',
+  tableName: 'Categories',
 })
 export class Category extends Model<Category> {
 
