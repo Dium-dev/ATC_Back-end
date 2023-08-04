2e3430c32d95b1e57c92209c8f3c9394ed381314
Fabian Romero <chapulinozeta@gmail.com>
2023-08-04 11:02:50 -0400

    Agregué una columna 'Id' de tipo UUID

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -7,6 +7,14 @@ import { Table, Model, Column, DataType } from 'sequelize-typescript';
 export class Category extends Model<Category> {
 
   @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  })
+    id: string;
+
+  @Column({
     type:DataType.STRING,
     allowNull:false,
     unique:true,
