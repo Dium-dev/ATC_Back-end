5a7690e759923d6e0c4e8c61f1667de7afa952b8
Fabian Romero <chapulinozeta@gmail.com>
2023-08-07 19:18:57 -0400

    Create the create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,42 @@
-export class CreateUserDto {}
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, IsNotEmpty, Equals } from 'class-validator';
+
+enum Rol {
+  superAdmin = 'superAdmin',
+  admin = 'admin',
+  user = 'user',
+}
+
+export class CreateUserDto {
+
+  @ApiProperty()
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+    email: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+    password: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+    firtsName: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+    lastName: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+    phone: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+    rol: string;
+}
