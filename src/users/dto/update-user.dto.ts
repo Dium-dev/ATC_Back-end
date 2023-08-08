303cd356ade8e802ca83b9755b9a2829181c2681
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-08-08 13:55:26 -0300

    modifique el update-user.dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,5 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto{
     firtsName: string
     lastName: string
     email: string
