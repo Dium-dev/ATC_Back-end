175f95c366f251a36d97f6b539e3412f77b89e0c
Fabian Romero <chapulinozeta@gmail.com>
2023-08-08 11:45:01 -0400

    Delete 'IsString' decorator

--- src/users/dto/get-user.dto.ts
+++ src/users/dto/get-user.dto.ts
@@ -3,7 +3,6 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class GetUserDto {
 
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-  @IsString({ message: 'El campo $property debe ser un texto' })
   @IsEmail(undefined, { message: 'Invalid email format' })
     email:string;
 
