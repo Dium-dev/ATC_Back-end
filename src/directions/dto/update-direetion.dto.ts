b0b534f20aaa5e40bf6d27596d2363278fb8c253
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-08-08 19:12:32 -0300

    modifique de acuerdo a los comentarios

--- src/directions/dto/update-direetion.dto.ts
+++ src/directions/dto/update-direetion.dto.ts
@@ -1,8 +1,21 @@
+import { IsString, IsOptional, IsNumber } from "class-validator"
+
 export class UpdateDireetionDto{
 
+    @IsOptional()
+    @IsNumber()
     codigoPostal: number
+
+    @IsOptional()
+    @IsString()
     ciudad: string
+
+    @IsOptional()
+    @IsString()
     estado: string
+
+    @IsOptional()
+    @IsString()
     calle: string
 
 
