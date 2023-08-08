5c90d2df6e605ed1ec61342713ca7644fa7ad4b4
Fabian Romero <chapulinozeta@gmail.com>
2023-08-07 20:09:03 -0400

    nuevo dto para el inicio de sesion

---
+++ src/users/dto/get-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class GetUserDto {
+
+  @IsNotEmpty({ message: 'El campo $property está vacío' })
+  @IsString({ message: 'El campo $property debe ser un texto' })
+  @IsEmail(undefined, { message: 'Invalid email format' })
+    email:string;
+
+  @IsNotEmpty({ message: 'El campo $property está vacío' })
+  @IsString({ message: 'El campo $property debe ser un texto' })
+    password:string;
+}
