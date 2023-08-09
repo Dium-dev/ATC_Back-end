c081afebf1482bafcd99f6916340cb8b84413cbf
EsteChar2906 <ccarlospachecoc@gmail.com>
2023-08-09 11:15:05 -0500

    Formato de contraseña

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,32 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
-
+import { IsString, IsEmail, IsNotEmpty, Matches } from 'class-validator';
 
 export class CreateUserDto {
-
   @ApiProperty()
   @IsEmail(undefined, { message: 'Invalid email format' })
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-    email: string;
+  email: string;
 
   @ApiProperty()
   @IsString({ message: 'El campo $property debe ser un texto' })
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-    password: string;
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&_])[A-Za-z\d$@$!%*?&]{8,15}/,
+  )
+  password: string;
 
   @ApiProperty()
   @IsString({ message: 'El campo $property debe ser un texto' })
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-    firstName: string;
+  firstName: string;
 
   @ApiProperty()
   @IsString({ message: 'El campo $property debe ser un texto' })
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-    lastName: string;
+  lastName: string;
 
   @ApiProperty()
   @IsString({ message: 'El campo $property debe ser un texto' })
   @IsNotEmpty({ message: 'El campo $property está vacío' })
-    phone: string;
-
+  phone: string;
 }
