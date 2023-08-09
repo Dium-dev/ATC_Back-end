618b3ec1f181a1c4a09107795c312c7f37513080
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-08-09 17:14:20 -0300

    validacion de datos para crear usuario

--- src/directions/dto/create-direetion.dto.ts
+++ src/directions/dto/create-direetion.dto.ts
@@ -1,1 +1,21 @@
-export class CreateDireetionDto {}
+import {IsString, IsInt} from 'class-validator'
+
+export class CreateDireetionDto {
+   
+    @IsInt({ message: 'El código postal debe ser un número entero' })
+    codigoPostal: number
+
+    
+    @IsString({ message: 'La ciudad debe ser un string' })
+    ciudad: string
+
+    
+    @IsString({ message: 'El estado debe ser un string' })
+    estado: string
+
+   
+    @IsString({ message: 'La calle debe ser un string' })
+    calle: string
+
+
+}
