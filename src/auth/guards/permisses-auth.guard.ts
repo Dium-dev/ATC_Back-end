f0fa8a4fc5e43a8b1c68a662c24c1d8339605bd3
Fabian Romero <chapulinozeta@gmail.com>
2023-08-11 18:49:06 -0400

    New Guard for claims

---
+++ src/auth/guards/permisses-auth.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { PERMISS_KEY, Permiss } from 'src/utils/custom/permiss.decorator';
+import { CaslAbilityFactory } from '../../casl/casl-ability.factory/casl-ability.factory';
+import { ForbiddenError } from '@casl/ability';
+
+@Injectable()
+export class PermissGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private caslAbilityFactory: CaslAbilityFactory ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    // Getting metadata from @Permisses decorator
+    const requiredPermisses = this.reflector.getAllAndOverride<Permiss[]>(PERMISS_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredPermisses.length) {
+      return true;
+    }
+    // user object provided by RolesGuard. 
+    // user: {sub: user.id, username: user.firstname, rol:user.rol}
+    const { user } = context.switchToHttp().getRequest();
+    const ability = this.caslAbilityFactory.createForUser(user);
+
+    //Checking if user is authorized
+    try {
+      requiredPermisses.forEach(permiss => {
+        ForbiddenError.from(ability)
+          .setMessage('This user is not authorized to perform this action')
+          .throwUnlessCan(permiss.action, permiss.subject);
+      });
+
+      return true;
+
+    } catch (error) {
+      throw new ForbiddenException(error.message);
+    }
+    
+  }
+}
