c6976b18439d75a553e585c8abb164f4a9867e2d
Fabian Romero <chapulinozeta@gmail.com>
2023-08-15 19:36:20 -0400

    Created sendMail method

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { transporter } from '../utils/mailer/mailer';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+
+  async sendMails(data:any, topic:string):Promise<void> {
+    switch (topic) {
+      case 'RESET_PASSWORD':
+        transporter.sendMail({
+          to: 'correodeprueba', //Coloca tu dirección de correo si estás haciendo pruebas
+          subject:'Reseteo de contraseña',
+          html:'<h1>Hola wakakakakak</h1>',
+        });
+      default:
+        return;
+    }
+  }
+}
