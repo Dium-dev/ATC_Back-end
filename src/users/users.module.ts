07672216f15082a8ed67bc61e6c77ffa8dbef256
Fabian Romero <chapulinozeta@gmail.com>
2023-08-15 19:32:46 -0400

    Added MailsService as a Provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,13 +6,14 @@ import { UsersService } from './users.service';
 import { Direction } from '../directions/entities/direction.entity';
 import { AuthModule } from '../auth/auth.module';
 import { AuthService } from 'src/auth/auth.service';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [
     forwardRef(() => AuthModule),
     SequelizeModule.forFeature([User, Direction]),
   ],
-  providers: [UsersService, AuthService],
+  providers: [UsersService, AuthService, MailService],
   controllers: [UsersController],
   exports: [UsersService],
 })
