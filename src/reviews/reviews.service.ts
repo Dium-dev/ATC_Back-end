0079a6d4de8c7d8c2ef566a1ceceb49e7367dfc1
Fabian Romero <chapulinozeta@gmail.com>
2023-08-22 13:14:26 -0400

    New way to create Reviews and new methods

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -2,33 +2,79 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { Review } from './entities/review.entity';
+import { IReview } from './interfaces/getReviews.interface';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ReviewsService {
 
-  async create(createReviewDto: CreateReviewDto):Promise<string> {
+  async create(id:string, createReviewDto: CreateReviewDto):Promise<string> {
     try {
-      const { userId, review, rating } = createReviewDto;
-      await Review.create({ review, rating, userId });
+      const user = User.findOne({
+        where:{
+          id:id,
+        },
+      });
+      (await user).$create('review', createReviewDto);
       return 'Created review successfully';
     } catch (error) {
       throw new BadRequestException(error.message);
     }
   }
 
-  findAll() {
-    return `This action returns all reviews`;
+  async findAll():Promise<Review[]> {
+    try {
+      const reviews = await Review.findAll({
+        include:{
+          model: User,
+          attributes: ['firstName', 'lastName'],
+        },
+        attributes:[ 'review', 'rating', 'updatedOn' ],
+        where:{
+          active: true,
+        },
+      });
+      return reviews;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  async update(id: string, updateReviewDto: UpdateReviewDto):Promise<Review> {
+    try {
+      //Update
+      await Review.update(updateReviewDto, {
+        where: {
+          id: id,
+        },
+      });
+      //Get review
+      const newReview = await Review.findOne({
+        include: {
+          model: User,
+          attributes:['firstName', 'lastName'],
+        },
+        where:{
+          id: id,
+        },
+      });
+      return newReview;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
   }
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
-
-  remove(id: number) {
+  async removeOrActivate(id: string, activate: boolean):Promise<string> {
+    try {
+      await Review.update({ active: activate }, {
+        where:{
+          id: id,
+        },
+      });
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
     return `This action removes a #${id} review`;
   }
+
 }
