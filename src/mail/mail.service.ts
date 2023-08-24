7c0a0bb8357096a77c2f68b6486ded82d0a75ba6
Fabian Romero <chapulinozeta@gmail.com>
2023-08-24 13:04:08 -0400

    Haciendo cambios en el mailer

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,39 +1,36 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, HttpException } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { IResponse } from 'src/utils/interfaces/response.interface';
-import { IContext } from './interfaces/context.interface';
+import { SendMailDto } from './dto/sendMail.dto';
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async sendMails(
-    to: string,
-    subject: string,
-    template: string,
-    context: IContext,
-  ): Promise<IResponse> {
-    switch (subject) {
-      case 'RESET_PASSWORD':
-        const mail = await this.mailerService.sendMail({
-          to,
-          subject:
-            'Se ha solicitado recuperar la contraseña de tu cuenta en ATC',
-          template: `./${template}`,
-          context,
-        });
+  //Si no hay un contexto, simplemente pon '{}'
+  async sendMails(sendMailDto: SendMailDto): Promise<IResponse> {
+    const { addressee, subject, context } = sendMailDto;
+    try {
+      let mail;
+      switch (subject) {
+        case 'RESET_PASSWORD':
+          mail = await this.mailerService.sendMail({
+            to: addressee,
+            subject: 'Se ha solicitado recuperar la contraseña de tu cuenta en ATC',
+            template: './recoverPassword', //`./${template}`,
+            context,
+          });
+          break;
 
-        if (mail) {
-          return {
-            statusCode: 200,
-            message: 'El link para recuperar la contraseña ha sido enviado',
-          };
-        } else {
-          throw new BadRequestException(
-            'Error al enviar el correo de recuperación',
-          );
-        }
-      default:
-        return;
+          
+      }
+      //If success
+      if (mail) return { statusCode: 200, message: 'El link para recuperar la contraseña ha sido enviado' };
+      //Else
+      throw new InternalServerErrorException('Error al enviar el correo de recuperación');
+    } catch (error) {
+      throw new HttpException(error.message, error.status);
     }
+    
+
   }
 }
