b148e196f21408e20a011f9f2fc07e24c5926515
sofiavictoriafranco <sofiavictoriafranco@hotmail.com>
2023-08-30 21:52:53 -0300

    ruta obtener orden por id

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
@@ -14,6 +15,34 @@ export class OrdersService {
 
   findOne(id: number) {
     return `This action returns a #${id} order`;
+  }
+
+  async findOneOrder(id: string) {
+
+    try {
+      const order = await Order.findByPk(id);
+
+      if (order) {
+        return {
+          statusCode: 200,
+          order,
+        };
+      } else {
+        throw new NotFoundException(
+          'Orden no encontrada',
+        );
+      }
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw new NotFoundException(
+          'Orden no encontrada',
+        );
+      } else {
+        throw new InternalServerErrorException('Error del servidor');
+      }
+    }
+    
+
   }
 
   update(id: number, updateOrderDto: UpdateOrderDto) {
