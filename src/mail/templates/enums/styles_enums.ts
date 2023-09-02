203a5a647b6bb9ea802876d78764e52f2acae472
Fabian Romero <chapulinozeta@gmail.com>
2023-09-02 14:57:46 -0400

    New enum for templates' styles

---
+++ src/mail/templates/enums/styles_enums.ts
@@ -0,0 +1,6 @@
+export enum Styles {
+  header = 'text-align:center; padding-top:4vh; padding-bottom:4vh; background-color:white;',
+  imgLogo = ' max-width:100%; align-content:center;',
+  articleContent = 'border-bottom:solid black 1px; padding-bottom:4vh;',
+  textStyles = 'color:black; text-align:left',
+}
