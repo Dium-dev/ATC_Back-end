b4ddd6680ae3c9752b9e74ec366f77995ff5cc10
Fabian Romero <chapulinozeta@gmail.com>
2023-09-06 14:18:30 -0400

    Primeras columnas de la tabla payments

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,1 +1,50 @@
-export class Payment {}
+import { Column, CreatedAt, DataType, Model, Table } from 'sequelize-typescript';
+
+export enum PaymentState {
+  DONE = 'done',
+  PENDING = 'pending',
+  FAILED = 'failed',
+}
+
+@Table({
+  tableName: 'Payments',
+  underscored: true,
+  timestamps: true,
+  paranoid: true,
+})
+export class Payment extends Model<Payment> {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  })
+    id: string;
+    
+  @Column({
+    type: DataType.NUMBER,
+    allowNull:false,
+  })
+    amount: number;
+
+  //Esto podría cambiar después
+  @Column({
+    type: DataType.ENUM(...Object.values(PaymentState)),
+    allowNull: false,
+    defaultValue: PaymentState.PENDING,
+  })
+    payment_state: PaymentState;
+
+  //Esto puede cambiar después
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+    user_email: string;
+
+  @CreatedAt
+  @Column({
+    type:DataType.DATEONLY,
+  })
+    creationDate: Date;
+}
