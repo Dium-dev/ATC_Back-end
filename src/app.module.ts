38d542062ffb61f3f3d9bc39dfa9e5605c70c488
Lautaro Garcia <lautarogonzalogarcia@outlook.com>
2023-09-07 10:15:19 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -56,7 +56,7 @@ import { Order } from './orders/entities/order.entity';
       autoLoadModels: true,
       synchronize: true,
       logging: false,
-      sync: { force: false },
+      sync: { force: true },
     }),
 
     ProductsModule,
