ec9ba27a0b7a3f9e8dda46084ee85c1b78a45515
Fabian Romero <chapulinozeta@gmail.com>
2023-09-11 15:11:14 -0400

    Deleted unnecessary methods

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -8,17 +8,7 @@ import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.ordersService.findAll();
-  }
-
-
+  //Obtener una orden por id
   @ApiOperation({ summary: 'Obtener orden' })
   @ApiResponse({
     status: 200,
@@ -32,24 +22,9 @@ export class OrdersController {
     type: 'string',
   })
   @Get(':id')
-  findOneOrder(@Param('id') id: string) {
-    const response = this.ordersService.findOneOrder(id);
+  async findOneOrder(@Param('id') id: string) {
+    const response = await this.ordersService.findOneOrder(id);
     return response;
   }
 
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const response = await this.ordersService.findOne(id);
-    return response;
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
-    return this.ordersService.update(+id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ordersService.remove(+id);
-  }
 }
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -6,17 +6,6 @@ import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
-  }
-
-  findAll() {
-    return 'This action returns all orders';
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
-  }
 
   async findOneOrder(id: string) {
 
@@ -58,11 +47,4 @@ export class OrdersService {
 
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} order`;
-  }
 }
