eabc074bfb7c033f7433a38ee30244ef32f7800c
Fabian Romero <chapulinozeta@gmail.com>
2023-09-12 21:01:35 -0400

    Nuevo dto para orders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,9 @@
-export class CreateOrderDto {}
+import { AmountPricePerProduct } from '../interfaces/amount-price-product';
+
+export class CreateOrderDto {
+  total: number;
+
+  userId: string;
+
+  products: AmountPricePerProduct[];
+}
