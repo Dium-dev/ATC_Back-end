159567ebb4c9cd4917a1918f3fff3c2aba7478c2
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-09-18 14:19:58 -0300

    getCartProducts new param cartId vs prev userId

--- src/shopping-cart/shopping-cart.service.ts
+++ src/shopping-cart/shopping-cart.service.ts
@@ -188,32 +188,27 @@ export class ShoppingCartService {
     }
   }
 
-  async getCartProducts(userId: string) {
+  async getCartProducts(cartId: string) {
     try {
-      const thisCart = await ShoppingCart.findOne({
-        where: { userId },
-        attributes: ['id'],
+      const thisCart = await ShoppingCart.findByPk(cartId, {
         include: [{ model: Product, attributes: ['id', 'title', 'price'] }],
       });
-
+  
       if (!thisCart) {
-        throw new NotFoundException(
-          'No se encontró el carrito de compras para el usuario.',
-        );
+        throw new NotFoundException('No se encontró el carrito de compras.');
       }
-
+  
       const products = await Promise.all(
         thisCart.products?.map(async (product) => {
-          // Aquí obtenemos la cantidad de productos en el carrito para este producto específico
           const cartProduct = await CartProduct.findOne({
             where: {
               cartId: thisCart.id,
               productId: product.id,
             },
           });
-
+  
           const subtotal = product.price * cartProduct.amount;
-
+  
           return {
             id: product.id,
             title: product.title,
@@ -221,15 +216,11 @@ export class ShoppingCartService {
             amount: cartProduct.amount,
             subtotal, // Agregar el subtotal para este producto
           };
-        }),
+        })
       );
-
-      // Calcula el total como la suma de todos los subtotales
-      const total = products.reduce(
-        (acc, product) => acc + product.subtotal,
-        0,
-      );
-
+  
+      const total = products.reduce((acc, product) => acc + product.subtotal, 0);
+  
       return {
         id: thisCart.id,
         products,
@@ -240,7 +231,7 @@ export class ShoppingCartService {
         throw new NotFoundException(error.message);
       } else {
         throw new InternalServerErrorException(
-          'Error del servidor al obtener el carrito de compras.',
+          'Error del servidor al obtener el carrito de compras.'
         );
       }
     }
