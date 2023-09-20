159cefc0fd7aff999cde23e97f9f2a605d5a59f5
Fabian Romero <chapulinozeta@gmail.com>
2023-09-20 17:38:36 -0400

    New interface for responses

--- src/orders/interfaces/response-order.interface.ts
+++ src/orders/interfaces/response-order.interface.ts
@@ -1,6 +1,17 @@
+import { IGetProducts } from 'src/products/interfaces/getProducts.interface';
 import { Order } from '../entities/order.entity';
 
 export interface IOrder {
   statusCode: number;
   data: Order | Order[] | string;
+}
+
+export interface IGetOrders {
+  statusCode: number;
+  data: {
+    orders: Order[],
+    totalOrders: number,
+    totalPages: number,
+    page: number
+  }
 }
