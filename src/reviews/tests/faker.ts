cba12b7ed421b5ac5c14d2d1a4833dddd1e9686f
Fabian Romero <chapulinozeta@gmail.com>
2023-10-09 17:02:34 -0400

    New fake data factory

---
+++ src/reviews/tests/faker.ts
@@ -0,0 +1,34 @@
+import { faker } from '@faker-js/faker';
+import { UserChangePasswordDto } from 'src/auth/dto/user-change-password.dto';
+import { CreateReviewDto } from '../dto/create-review.dto';
+import { Rating, Review } from '../entities/review.entity';
+
+//Generates the kind of user data that is extracted from JWT (check the dto)
+export const createUserObject = ():UserChangePasswordDto => {
+  return {
+    userId:faker.string.uuid(),
+    username:faker.internet.email(),
+  };
+};
+
+//Generates data about a fake review
+export const generateFakeReviewData = (review = faker.lorem.sentence(), rating = Rating.five ):CreateReviewDto => {
+  return {
+    review:review,
+    rating:rating,
+  };
+};
+
+//Create false review instance
+export const newReview = (review: CreateReviewDto, userId:string) => {
+  const fakeReview = {
+    review: review.review,
+    rating: review.rating,
+    id: userId,
+    active: true,
+    /* creationDate: faker.date.anytime(),
+    updatedOn: faker.date.anytime(), */
+  };
+
+  return fakeReview;
+};
