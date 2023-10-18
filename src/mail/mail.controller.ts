c0d90bc6ea4b3abcc969622f8c2996ba65b47f07
Celes Flowers <seleccionfloresceleste@gmail.com>
2023-10-18 13:56:36 -0300

    controller mail

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Post, Body, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { MailService } from './mail.service';
+import{IContactFormAdminContext} from './interfaces/contact-form-admin-context.interface';
+import{IContactFormUserContext} from './interfaces/contact-form-user-cotext.interface';
+
 
 @Controller('contact')
 export class ContactController {
@@ -8,25 +11,29 @@ export class ContactController {
 
   @Post()
   async sendContactForm(@Body() contactData: any, @Res() res: Response) {
-    // Aquí procesas los datos del formulario y luego llamas a MailService para enviar los correos electrónicos.
-    // Debes ajustar contactData para que coincida con los datos que esperas en el formulario.
-
     try {
-      // Envía un correo al usuario informando que recibieron su consulta.
-      const userContext = {
-        // Define el contexto del correo para el usuario.
+      const userContext: IContactFormUserContext = {
+        firstname: contactData.firstname,
       };
-      await this.mailService.sendContactFormEmail(contactData.userEmail, userContext);
+      await this.mailService.sendMails({
+        addressee: contactData.email, 
+        subject: 'CONTACT_FORM_USER', 
+        context: userContext,
+      });
 
-      // Envía un correo al administrador informando que tienen una consulta para responder.
-      const adminContext = {
-        // Define el contexto del correo para el administrador.
+      const adminContext: IContactFormAdminContext = {
+        firstname: contactData.firstname, 
+        contactInfo: contactData.email, 
+        message: contactData.message,
       };
-      await this.mailService.sendContactFormEmail('correo_del_administrador@example.com', adminContext);
+      await this.mailService.sendMails({
+        addressee: 'correo_del_administrador@example.com', 
+        subject: 'CONTACT_FORM_ADMIN', //
+        context: adminContext,
+      });
 
       return res.status(200).json({ message: 'Formulario de contacto enviado con éxito' });
     } catch (error) {
-      // Manejo de errores
       return res.status(error.status).json({ error: error.message });
     }
   }
