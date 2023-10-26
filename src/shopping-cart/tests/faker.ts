2f5d5d20e3957b7d9fb971125df278c12d07f1a3
Fabian Romero <chapulinozeta@gmail.com>
2023-10-26 18:31:14 -0400

    Added some new fakers

--- src/shopping-cart/tests/faker.ts
+++ src/shopping-cart/tests/faker.ts
@@ -1,2 +1,27 @@
 import { faker } from '@faker-js/faker';
+import { stateproduct } from 'src/products/entities/product.entity';
 
+export const generatesShoppingCartInstance = (userId:string) => {
+  return {
+    id: faker.string.uuid(),
+    userId:userId,
+  };
+};
+
+export const generatesProduct = (productId:string, state:stateproduct) => {
+  return {
+    id: productId,
+    state: state,
+    /*     title: faker.commerce.product(),
+    description: faker.commerce.productDescription(), */
+    stock: faker.number.int({ min:100 }),
+    price: faker.number.float({ min: 0, max: 100, precision: 0.01 }),
+  };
+};
+
+export const generatesCartProduct = (data) => {
+  return {
+    id:faker.string.uuid(),
+    ...data,
+  };
+};
