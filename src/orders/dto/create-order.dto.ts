43c39b598d59ce45650e42c3681918f452339302
lechugaxthz <lautarogonzalogarcia@outlook.com>
2024-01-11 14:59:03 -0300

    ajustes sobre create de orders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,9 +1,11 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsUUID } from 'class-validator';
 
 export class CreateOrderDto {
   @IsUUID('4', { message: 'Se debe proporcionar el Id del usuario' })
-  @IsNotEmpty({ message: 'El campo $property no debe estar vacio' })
-  userId: string;
+  @IsOptional({ message: 'El campo $property no debe estar vacio' })
+  /*El campo es opcional porque en cierta instancia (controller) no es requerida del body, 
+  en cambio en el service se indica como tipo de dato del create*/
+  userId?: string;
 
   @IsUUID('4', {
     message: 'Recuerda mandar el Id de la direccion seleccionada',
