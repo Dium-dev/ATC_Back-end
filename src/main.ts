1618ad8643b1a98a48bb9f7ea29e09ef21b4a7d6
Lautaro Garcia <lautarogonzalogarcia@outlook.com>
2024-01-15 10:20:43 -0300

    correccion de cores

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: FRONT_HOST,
-    methods: ['GET', 'POST', 'PATHC', 'DELETE'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders:['Authorization'],
     credentials: true,
   });
